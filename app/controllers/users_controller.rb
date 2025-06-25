651a84f7fd5f44c95c6c8801e9a565acfb9de981
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-06-25 23:17:03 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    # Assuming you have a User model and a corresponding view for creating a new user 
   end
 
   def show
     @user = User.find(params[:id])
     # Assuming you have a User model and a corresponding view for showing user details
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      # Adjust the permitted parameters based on your User model attributes
+    end
 
 end
