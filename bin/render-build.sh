f9051c0e530e9340212460b44651e3355746e1f2
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-12 22:25:12 +0900

    Update render-build script 2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,15 +5,8 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# 他のセッションを強制終了（安全にリセットするため）
-bundle exec rails runner "ActiveRecord::Base.connection.execute(%{
-  SELECT pg_terminate_backend(pid)
-  FROM pg_stat_activity
-  WHERE datname = '#{ActiveRecord::Base.connection.current_database}'
-    AND pid <> pg_backend_pid();
-})"
 
-# 強制的にDBを再作成（練習用だからOK）
+# `reset` はやめて、明示的に drop → create → migrate → seed
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 bundle exec rails db:create
 bundle exec rails db:migrate
