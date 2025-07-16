bfb5ed85cead461187878c151c4c71b34f401d33
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-16 11:10:03 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# メインのサンプルユーザーを1人作成する
-User.find_or_create_by!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobarrr",
-             password_confirmation: "foobarrr",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
+# メインのサンプルユーザー
+User.find_or_create_by!(email: "example@railstutorial.org") do |user|
+  user.name                  = "Example User"
+  user.password              = "foobarrr"
+  user.password_confirmation = "foobarrr"
+  user.admin                 = true
+  user.activated             = true
+  user.activated_at          = Time.zone.now
+end
 
-# 追加のユーザーをまとめて生成する
+# 追加ユーザー
 99.times do |n|
-  name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.find_or_create_by!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
+  User.find_or_create_by!(email: email) do |user|
+    user.name                  = Faker::Name.name
+    user.password              = "password"
+    user.password_confirmation = "password"
+    user.activated             = true
+    user.activated_at          = Time.zone.now
+  end
 end
