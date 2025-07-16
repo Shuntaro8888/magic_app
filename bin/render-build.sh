69a4f33e7ef75e24b8f536f406016df0d4db94e7
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-16 11:02:27 +0900

    Update seeds.rb

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,16 +6,16 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # 他の接続をkill（psqlコマンドが使えれば）
-psql "${DATABASE_URL}?sslmode=require" -c \
-  "SELECT pg_terminate_backend(pid) \
-   FROM pg_stat_activity \
-   WHERE datname = current_database() AND pid <> pg_backend_pid();"
+# psql "${DATABASE_URL}?sslmode=require" -c \
+#  "SELECT pg_terminate_backend(pid) \
+#   FROM pg_stat_activity \
+#   WHERE datname = current_database() AND pid <> pg_backend_pid();"
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate db:seed
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate db:seed
 
-# bundle exec rails db:migrate
+bundle exec rails db:migrate
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-# bundle exec rails db:seed
+bundle exec rails db:seed
 
 # renderでデプロイ失敗するので `reset` はやめて、明示的に drop → create → migrate → seed
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
