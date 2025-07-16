aa6e1c732a00a9a3ff9a4659dac6b37351a1e727
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-16 10:37:53 +0900

    Update render-build script 5

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,7 +7,7 @@ bundle exec rails assets:clean
 
 
 bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
 
 # renderでデプロイ失敗するので `reset` はやめて、明示的に drop → create → migrate → seed
