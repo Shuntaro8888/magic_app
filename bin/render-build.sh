d5177470d898f3a0d6341365236651a59d154180
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-16 10:48:03 +0900

    Update render-build script 6

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,10 +5,17 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
+# 他の接続をkill（psqlコマンドが使えれば）
+psql "${DATABASE_URL}?sslmode=require" -c \
+  "SELECT pg_terminate_backend(pid) \
+   FROM pg_stat_activity \
+   WHERE datname = current_database() AND pid <> pg_backend_pid();"
 
-bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate db:seed
+
+# bundle exec rails db:migrate
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+# bundle exec rails db:seed
 
 # renderでデプロイ失敗するので `reset` はやめて、明示的に drop → create → migrate → seed
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
