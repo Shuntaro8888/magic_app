4910f463b862f4a9495c98e0bb6d1c81b99dc657
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-24 15:27:10 +0900

    Add Image posting func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper  # Assuming you have a SessionsHelper module for session management
+
+  private
+  # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other # Redirects to login page with HTTP 303 status
+      end
+    end
 end
