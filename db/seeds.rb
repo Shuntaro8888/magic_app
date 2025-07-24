c007ef582581551c236b085d85be76d5a9548cac
Shuntaro8888 <ryotaro20250303@gmail.com>
2025-07-24 17:07:11 +0900

    Modify db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,5 +32,5 @@ end
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content) }
+  users.each { |user| user.microposts.find_or_create_by(content: content) }
 end
